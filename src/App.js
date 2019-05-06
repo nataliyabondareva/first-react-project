fc89576db9565e151ce1befaf0a9f7a77f082bba
tallybee <talbondareva@gmail.com>
2019-05-06 12:28:41 +0200

    change stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the world
+          Welcome to Codaisseur
         </p>
         <a
           className="App-link"
